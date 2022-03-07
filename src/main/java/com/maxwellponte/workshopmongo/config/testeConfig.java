feeb180cd0c5bf6e2e48beeacf7309088cfede69
bloodofpride <antonio_maxwell@hotmail.com>
2022-03-07 08:25:35 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/maxwellponte/workshopmongo/config/testeConfig.java
+++ src/main/java/com/maxwellponte/workshopmongo/config/testeConfig.java
@@ -3,6 +3,7 @@ package com.maxwellponte.workshopmongo.config;
 import com.maxwellponte.workshopmongo.domain.Post;
 import com.maxwellponte.workshopmongo.domain.User;
 import com.maxwellponte.workshopmongo.dtos.AuthorDTO;
+import com.maxwellponte.workshopmongo.dtos.CommentDTO;
 import com.maxwellponte.workshopmongo.repositories.PostRepository;
 import com.maxwellponte.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,14 @@ public class testeConfig implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1,post2));
